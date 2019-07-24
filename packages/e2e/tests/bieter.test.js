767b370bb52f06550f6c91bebc826884fa71b043
Mike Bild <mike.bild@googlemail.com>
2019-07-24 10:30:30 +0200

    make it more configuable

--- packages/e2e/tests/bieter.test.js
+++ packages/e2e/tests/bieter.test.js
@@ -1,24 +1,30 @@
+const username = process.env.USERNAME_BID
+const password = process.env.PASSWORD_BID
+const baseUrl = process.env.BASE_URL
+
 describe('Bieter User - Standardansicht', () => {
   beforeAll(async () => {
-    await page.goto('http://staging-van-ham.auctionng.de/login.html', { waitUntil: 'networkidle2' })
+    await page.goto(`${baseUrl}/login.html`, { waitUntil: 'networkidle2' })
   })
 
   afterAll(async () => {
-    await page.goto('http://staging-van-ham.auctionng.de/logout.html', { waitUntil: 'networkidle2' })
+    await page.goto(`${baseUrl}/logout.html`, { waitUntil: 'networkidle2' })
   })
-  it('Benutzername "sk@onsite.org" mit gültigem Passwort sollte sich anmelden können und auf der /my_activity.html geleitet werden', async () => {
+
+  test('Benutzername "sk@onsite.org" mit gültigem Passwort sollte sich anmelden können und auf der /my_activity.html geleitet werden', async () => {
     const usernameInput = await page.$('#login_email')
-    await usernameInput.type('sk@onsite.org')
+    await usernameInput.type(username)
     const passwordInput = await page.$('#login_password')
-    await passwordInput.type('0CDKmROs')
+    await passwordInput.type(password)
 
     await Promise.all([
       page.evaluate(() => document.querySelector('#action').click()),
-      browser.waitForTarget(target => target.url() === 'http://staging-van-ham.auctionng.de/my_activity.html'),
+      browser.waitForTarget(target => target.url() === `${baseUrl}/my_activity.html`),
     ])
 
-    await page.goto('http://staging-van-ham.auctionng.de/live_bid_panel/index.php?language=de', { waitUntil: 'networkidle2' });
+    await page.goto(`${baseUrl}/live_bid_panel/index.php?language=de`, { waitUntil: 'networkidle2' });
 
     await page.evaluate(() => document.querySelector('#save_inet_bid').click());
-  })
+  }, 10000)
+
 })
--- packages/e2e/tests/gast.test.js
+++ packages/e2e/tests/gast.test.js
@@ -1,21 +1,23 @@
+const baseUrl = process.env.BASE_URL || 'http://staging-van-ham.auctionng.de'
+
 describe('Gast User - Standardansicht', () => {
   beforeAll(async () => {
-    await page.goto('http://staging-van-ham.auctionng.de/live_bid_panel/index.php?language=de', { waitUntil: 'networkidle2' })
+    await page.goto(`${baseUrl}/live_bid_panel/index.php?language=de`, { waitUntil: 'networkidle2' })
   })
 
-  it('sollte den Seitentitel "A:NG - Live Bidding 3.0 Beta 2" enthalten', async () => {
+  test('sollte den Seitentitel "A:NG - Live Bidding 3.0 Beta 2" enthalten', async () => {
     await expect(page.title()).resolves.toBe('A:NG - Live Bidding 3.0 Beta 2')
   })
 
-  it('sollte den Login Button "Einloggen zur Teilnahme" enthalten', async () => {
+  test('sollte den Login Button "Einloggen zur Teilnahme" enthalten', async () => {
     const loginBtnText = await page.$eval('#login_area a', e => e.innerText)
     await expect(loginBtnText).toBe('Einloggen zur Teilnahme')
   })
 
-  it('klick auf Login Button sollte die Login Page /login.html öffnen', async () => {
+  test('klick auf Login Button sollte die Login Page /login.html öffnen', async () => {
     await Promise.all([
       page.evaluate(() => document.querySelector('#login_area a').click()),
-      browser.waitForTarget(target => target.url() === 'http://staging-van-ham.auctionng.de/login.html?login_from_live_bid=1'),
+      browser.waitForTarget(target => target.url() === `${baseUrl}/login.html?login_from_live_bid=1`),
     ])
   })
 })
