12e33c14acdc1c147bdb2856214fc0bc13c6fdab
jp-onsite <jp@onsite.org>
2019-07-24 11:32:27 +0200

    auktionator Test

---
+++ packages/e2e/tests/auktionator.test.js
@@ -0,0 +1,30 @@
+const username = process.env.USERNAME_AUKTIONATOR
+const password = process.env.PASSWORD_AUKTIONATOR
+const baseUrl = process.env.BASE_URL
+
+describe('Bieter User - Standardansicht', () => {
+  beforeAll(async () => {
+    jest.setTimeout(20000)
+    await page.goto(`${baseUrl}/login.html`, { waitUntil: 'networkidle2' })
+  })
+
+  afterAll(async () => {
+    await page.goto(`${baseUrl}/logout.html`, { waitUntil: 'networkidle2' })
+  })
+
+  test(`Benutzername ${username} mit gültigem Passwort sollte sich anmelden können und auf der /my_activity.html geleitet werden`, async () => {
+    const usernameInput = await page.$('#login_email')
+    await usernameInput.type(username)
+    const passwordInput = await page.$('#login_password')
+    await passwordInput.type(password)
+
+    await Promise.all([
+      page.evaluate(() => document.querySelector('#action').click()),
+      browser.waitForTarget(target => target.url() === `${baseUrl}/my_activity.html`),
+    ])
+
+    await page.goto(`${baseUrl}/live_bid_panel/index.php?language=de`, { waitUntil: 'networkidle2' });
+
+    await page.evaluate(() => document.querySelector('#bidder_paddle2_1').click());
+  })
+})
