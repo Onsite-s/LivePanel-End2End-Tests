59a98b305f8014b392130bc7fafbd8ae6f6e0537
Mike Bild <mike.bild@googlemail.com>
2019-09-24 16:27:28 +0200

    adjust invoke output to improve human readability

--- packages/e2e/invoke.js
+++ packages/e2e/invoke.js
@@ -1,30 +1,42 @@
-const AWS = require('aws-sdk')
-const lambda = new AWS.Lambda()
-const [_, __, count = 1] = process.argv
+const AWS = require('aws-sdk');
+const lambda = new AWS.Lambda();
+const [_, __, count = 1] = process.argv;
 
-invokeE2E(parseInt(count))
+invokeE2E(parseInt(count));
 
 async function invokeE2E(count) {
-  const lambdaInvokeList = new Array(count)
-    .fill({})
-    .map(() => lambda.invoke({
-      FunctionName: 'e2e-puppeteer-function',
-      InvocationType: 'RequestResponse',
-      LogType: 'Tail',
-      Payload: JSON.stringify({})
-    })
+  const lambdaInvokeList = new Array(count).fill({}).map(() =>
+    lambda
+      .invoke({
+        FunctionName: 'e2e-puppeteer-function',
+        InvocationType: 'RequestResponse',
+        LogType: 'Tail',
+        Payload: JSON.stringify({}),
+      })
       .promise()
-      .catch(error => ({ LogResult: error.message, Payload: '{}' })))
+      .catch(error => ({ LogResult: error.message, Payload: '{}' }))
+  );
 
   try {
-    const lambdaResults = await Promise.all(lambdaInvokeList)
+    const lambdaResults = await Promise.all(lambdaInvokeList);
     lambdaResults.forEach(({ LogResult, Payload }) => {
-      const logResults = Buffer.from(LogResult, 'base64').toString('ascii')
-      const { results: { success, numFailedTests, numPassedTests, numPendingTests } } = JSON.parse(Payload)
-      console.log(logResults)
-      console.log({ success, numFailedTests, numPassedTests, numPendingTests })
-    })
+      const logResults = Buffer.from(LogResult, 'base64').toString('ascii');
+      const payload = JSON.parse(Payload);
+      const {
+        results: { success, numFailedTests, numPassedTests, numPendingTests, testResults },
+      } = JSON.parse(Payload);
+      console.log(logResults);
+      console.log('Erfolgreiche Ausführung aller Tests: ', success);
+      console.log('Erfolgreiche Tests: ', numPassedTests);
+      console.log('Nicht erfolgreiche Tests: ', numFailedTests);
+      console.log('Ausgesetzte Tests: ', numPendingTests);
+      console.log('Fehlermeldungen:');
+      testResults.forEach(x => {
+        console.log('Pfad:', x.testFilePath);
+        console.log('Fehler:', x.failureMessage);
+      });
+    });
   } catch (error) {
-    console.error(error)
+    console.error(error);
   }
 }
