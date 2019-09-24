cc975c2c12194d60b19824b708c62f1909a2b286
Mike Bild <mike.bild@googlemail.com>
2019-09-24 16:27:55 +0200

    fix test setup

--- packages/e2e/tests/auktionator.test stephan.js
+++ packages/e2e/tests/auktionator.test stephan.js
@@ -1,27 +1,44 @@
-const username = process.env.USERNAME_AUKTIONATOR
-const password = process.env.PASSWORD_AUKTIONATOR
-const baseUrl = process.env.BASE_URL
+const chrome = require('chrome-aws-lambda');
+
+const username = process.env.USERNAME_AUKTIONATOR;
+const password = process.env.PASSWORD_AUKTIONATOR;
+const baseUrl = process.env.BASE_URL;
 
 describe.skip('Bieter User - Standardansicht', () => {
+  let browser;
+  let page;
+
   beforeAll(async () => {
-    jest.setTimeout(20000)
-    await page.goto(`${baseUrl}/login.html`, { waitUntil: 'networkidle2' })
-  })
+    jest.setTimeout(20000);
+
+    browser = await chrome.puppeteer.launch({
+      args: chrome.args,
+      defaultViewport: chrome.defaultViewport,
+      executablePath: await chrome.executablePath,
+      headless: chrome.headless,
+      slowMo: 0,
+      devtools: true,
+      timeout: 0,
+    });
+    page = await browser.newPage();
+
+    await page.goto(`${baseUrl}/login.html`, { waitUntil: 'networkidle2' });
+  });
 
   afterAll(async () => {
-    await page.goto(`${baseUrl}/logout.html`, { waitUntil: 'networkidle2' })
-  })
+    await page.goto(`${baseUrl}/logout.html`, { waitUntil: 'networkidle2' });
+  });
 
   test(`Benutzername ${username} mit gültigem Passwort sollte sich anmelden können und auf der /my_activity.html geleitet werden`, async () => {
-    const usernameInput = await page.$('#login_email')
-    await usernameInput.type(username)
-    const passwordInput = await page.$('#login_password')
-    await passwordInput.type(password)
+    const usernameInput = await page.$('#login_email');
+    await usernameInput.type(username);
+    const passwordInput = await page.$('#login_password');
+    await passwordInput.type(password);
 
     await Promise.all([
       page.evaluate(() => document.querySelector('#action').click()),
       browser.waitForTarget(target => target.url() === `${baseUrl}/my_activity.html`),
-    ])
+    ]);
 
     await page.goto(`${baseUrl}/live_bid_panel/index.php?language=de`, { waitUntil: 'networkidle2' });
     await page.evaluate(() => document.querySelector('#save_hall_bid').click());
@@ -32,11 +49,11 @@ describe.skip('Bieter User - Standardansicht', () => {
     await delay(3000);
     await page.goto('about:blank');
     await delay(3000);
-  })
-})
+  });
+});
 
 function delay(timeout) {
   return new Promise(resolve => {
-    setTimeout(() => resolve(), timeout)
-  })
+    setTimeout(() => resolve(), timeout);
+  });
 }
